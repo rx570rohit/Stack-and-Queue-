73b9878e8115a62ea573b91866398c9886b9daea
rx570rohit <rohit.soni2018@vitbhopal.ac.in>
2022-05-10 22:14:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace LinkedList
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            // create Object of Implementing class
+            StackByLL obj = new StackByLL();
+
+            // insert Stack value
+            obj.push(11);
+            obj.push(22);
+            obj.push(33);
+            obj.push(44);
+        }
+    }
+}
---
+++ StackByLL.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class StackByLL
+    {
+        private class Node
+        {
+            public int data;
+
+            public Node link;
+        }
+
+       
+        Node top;
+
+        public StackByLL()
+        {
+            this.top = null;
+        }
+
+       
+  
+        public void push(int x)
+        {
+            Node temp = new Node();
+
+           
+            if (temp == null)
+            {
+                Console.Write("\nHeap Overflow");
+                return;
+            }
+
+            temp.data = x;
+
+            temp.link = top;
+
+            top = temp;
+        }
+        public bool isEmpty()
+        {
+            return top == null;
+        }
+
+    
+        public void display()
+        {
+            if (top == null)
+            {
+                Console.Write("\nStack Underflow");
+                return;
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+
+                   
+                    Console.Write("{0}->", temp.data);
+
+                    temp = temp.link;
+                }
+            }
+        }
+    }
+}
