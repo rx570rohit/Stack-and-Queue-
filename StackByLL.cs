1e75999024d9dbda4d72b70ce7d84d7eed370cc6
rx570rohit <rohit.soni2018@vitbhopal.ac.in>
2022-05-10 22:23:41 +0530

    final

--- StackByLL.cs
+++ StackByLL.cs
@@ -15,7 +15,7 @@ namespace LinkedList
             public Node link;
         }
 
-       
+
         Node top;
 
         public StackByLL()
@@ -23,13 +23,13 @@ namespace LinkedList
             this.top = null;
         }
 
-       
-  
+
+
         public void push(int x)
         {
             Node temp = new Node();
 
-           
+
             if (temp == null)
             {
                 Console.Write("\nHeap Overflow");
@@ -42,12 +42,41 @@ namespace LinkedList
 
             top = temp;
         }
+
         public bool isEmpty()
         {
             return top == null;
         }
 
-    
+        public int peek()
+        {
+            // check for empty stack
+            if (!isEmpty())
+            {
+                return top.data;
+            }
+            else
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+        }
+
+        // Utility function to pop top element from the stack
+        public void pop() // remove at the beginning
+        {
+            // check for stack underflow
+            if (top == null)
+            {
+                Console.Write("\nStack Underflow");
+                return;
+            }
+
+            // update the top pointer to
+            // point to the next node
+            top = (top).link;
+        }
+
         public void display()
         {
             if (top == null)
@@ -61,11 +90,19 @@ namespace LinkedList
                 while (temp != null)
                 {
 
-                   
+
                     Console.Write("{0}->", temp.data);
 
                     temp = temp.link;
                 }
+            }
+        }
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                peek();
+                pop();
             }
         }
     }
